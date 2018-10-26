beaba4f209c4c1619d78e988e1cb0a01d59cee0c
Daniel Han <hand052@newschool.edu>
2018-10-25 20:37:27 -0400

    update

---
+++ Han Daniel Asteroids Quiz/Assets/scripts/playerControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    public GameObject player;
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W)){
+            player.GetComponent<Rigidbody2D>().velocity = player.transform.up * 1;
+        }
+
+
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        //Ta Daaa
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 90));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("asteroids"))
+        {
+            Destroy(gameObject);
+
+        }
+    }
+
+
+}
---
+++ Han Daniel Asteroids Quiz/Assets/scripts/shooting.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class shooting : MonoBehaviour
+{
+    public GameObject laser;
+    public Transform laserPoint;
+
+
+
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var Laser = (GameObject)Instantiate(
+            laser,
+            laserPoint.position,
+            laserPoint.rotation);
+
+        // Add velocity to the bullet
+        Laser.GetComponent<Rigidbody2D>().velocity = Laser.transform.up * 6;
+
+        // Destroy the bullet after 2 seconds
+
+
+        Destroy(Laser, 2.0f);
+    }
+
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
